d5a6ccaf6731cac2b828fc1b172843fd709d5b0b
benjamin boganim <benjaminpert@gmail.com>
2022-01-24 14:31:45 +0000

    metod car controller

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,22 +1,51 @@
 class CarsController < ApplicationController
+before_action :set_car, except:[:index, :new, :create]
   def index
+    @cars = Cars.all
   end
 
   def show
   end
 
   def new
+    @car = Car.new
   end
 
   def create
-  end
+    @car = Car.new(car_params)
+    if @car.save
+      redirect_to car_path(@car)
+    else
+      render :new
+    end
 
-  def update
   end
 
   def edit
   end
 
-  def destroy
+  def update
+
+    @car.update(car_params)
+    redirect_to car_path(@car)
   end
+
+
+  def destroy
+
+     @car.destroy
+     redirect_to cars_path
+
+  end
+
+  private
+
+  def car_params
+    params.permit(:cars).require(:name, :model, :price)
+  end
+
+  def set_car
+     @car = Car.find(params[:id])
+  end
+
 end
