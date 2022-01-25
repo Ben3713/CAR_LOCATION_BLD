1875a38440f758521b0413215342b20fd4906c84
benjamin boganim <benjaminpert@gmail.com>
2022-01-25 12:20:39 +0000

    editbook

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,28 +1,37 @@
 class BookingsController < ApplicationController
-
- def new
+def new
   @booking = Booking.new
   @car = Car.find(params[:car_id])
- end
-
+end
+def edit
+  @car  = Car.find(params[:car_id])
+  @booking = Booking.find(params[:id])
+end
+def update
+  @car  = Car.find(params[:car_id])
+  @booking = Booking.find(params[:id])
+  @booking.update(booking_params)
+  redirect_to car_booking_path(@car, @booking)
+end
   def show
     @bookings = Booking.all
   end
-
   def create
     @booking = Booking.new(booking_params)
     @car = Car.find(params[:car_id])
     @booking.car = @car
     @booking.user = current_user
     if @booking.save
-      redirect_to car_booking_path(@car, @booking)
+      redirect_to car_booking_path(@car,@booking)
     else
       render :new
     end
   end
-
   def booking_params
     params.require(:booking).permit(:date)
   end
-
 end
+
+
+
+
